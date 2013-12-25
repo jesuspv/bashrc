89965df501b00d9a98e1e657d1d3640c678f59f2
Jesús Pardillo <dev@jesuspardillo.com>
2013-12-25 14:35:27 +0100

    updating install script

--- install.sh
+++ install.sh
@@ -4,11 +4,15 @@
 [[ "$1" =~ ^-(g|-git)$  ]] && { GITCONFIG=1; shift; }
 [[ "$1" == ""           ]] || { echo "error: unknown argument $1"; exit 1; }
 
-BASEDIR=$(dirname "`readlink -f $0`")
+BASEDIR=$(dirname "`readlink -f ${BASH_SOURCE[0]}`")
 FILES="alias bash_profile bashrc"
 
 for FILE in $FILES; do
-   test -s ~/.$FILE || (ln -s $BASEDIR/$FILE ~/.$FILE; echo "$FILE installed")
+   [[ -h ~/.$FILE ]] && unlink ~/.$FILE
+   [[ -f ~/.$FILE ]] && mv -f ~/.$FILE ~/.$FILE.last \
+                     && echo "$FILE backed up as $FILE.last"
+   ln -s $BASEDIR/$FILE ~/.$FILE
+   echo "$FILE installed"
 done
 
 test -n "$GITCONFIG" && ($BASEDIR/gitconfig; echo "gitconfig installed")
